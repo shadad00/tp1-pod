193ebe7d35d047e15be12e7942252db99ff0f0ef
shadad00 <shadad00@itba.edu.ar>
2022-09-07 10:53:55 -0300

    Maven archetype

---
+++ client/src/main/java/flightService/client/Client.java
@@ -0,0 +1,12 @@
+package flightService.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+    private static Logger logger = LoggerFactory.getLogger(Client.class);
+
+    public static void main(String[] args) {
+        logger.info("flightService Client Starting ...");
+    }
+}
