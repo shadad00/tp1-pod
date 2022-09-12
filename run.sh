3365b94e85ddc8755e63aa811ad7615a5122aa72
shadad00 <shadad00@itba.edu.ar>
2022-09-12 20:42:01 -0300

    Fixing script

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
-mvn clean install 
+#mvn clean install 
 cd server/target
 tar -xvf tp1-g14-server-1.0-SNAPSHOT-bin.tar.gz
 cd tp1-g14-server-1.0-SNAPSHOT/
-chmod u+x *.sh 
+chmod u+x run-server.sh run-registry.sh 
+./run-registry.sh& 
+./run-server.sh&
 cd ../../../client/target/
 tar -xvf tp1-g14-client-1.0-SNAPSHOT-bin.tar.gz
-chmod u+x *.sh
+chmod u+x *
 cd tp1-g14-client-1.0-SNAPSHOT/
 
